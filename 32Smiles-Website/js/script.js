4addcddd3009441ac8923113e76d099b6efe8ba8
Anita <writeanita.n@gmail.com>
2023-12-14 00:20:16 +0100

    added media query for a screen size of 1024px

--- 32Smiles-Website/js/script.js
+++ 32Smiles-Website/js/script.js
@@ -1,1 +1,20 @@
-hgasvtydvtyqwd
+function initMap() {
+    const maps = document.querySelectorAll('.location-map');
+    
+    maps.forEach(mapElement => {
+        const lat = parseFloat(mapElement.getAttribute('data-lat'));
+        const lng = parseFloat(mapElement.getAttribute('data-lng'));
+        
+        const map = new google.maps.Map(mapElement, {
+            center: { lat, lng },
+            zoom: 15 
+        });
+
+        
+        new google.maps.Marker({
+            position: { lat, lng },
+            map: map,
+            title: 'Dental Professional Location'
+        });
+    });
+}
